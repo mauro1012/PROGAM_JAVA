8066db20e400e8004c4da7e1aa7a5a331ff144c8
mauro1012 <mauricio.correa369@gmail.com>
2024-11-10 10:01:05 -0500

    first change in the code
    The program to commit the first repository on GitHub has been started

---
+++ Calculadora_github/src/main/java/com/mycompany/calculadora_github/Calculadora_github.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.calculadora_github;
+
+/**
+ *
+ * @author mauri
+ */
+public class Calculadora_github {
+
+    public static void main(String[] args) {
+        System.out.println("Hello ");
+        System.out.println("Choose the operation");
+        System.out.println("1. suma");
+        System.out.println("2. resta");
+        System.out.println("3. multiplicacion ");
+        System.out.println("4. divicion");
+    }
+}
